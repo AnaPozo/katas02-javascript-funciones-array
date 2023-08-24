a1756cd71301296cf64832b29a810658ea9ff828
Ana Pozo <the.analejandra@gmail.com>
2023-08-25 01:22:39 +0200

    test

--- tests/funcArray.test.js
+++ tests/funcArray.test.js
@@ -1,6 +1,6 @@
 import {describe, it} from 'node:test';
 import assert from 'node:assert/strict';
-import {maxOfTwoNumbers,
+import {maxOfTwoNumbers, 
   findLongestWord,
   sumArray,
   averageNumbers,
